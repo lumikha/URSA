4f4061d1a80290bf26d4cb060f8a79a79e5eadd5
jasper <jasper@lumikha.co>
2016-09-16 18:56:40 +0800

    Set "account" tab as default

--- header.php
+++ header.php
@@ -120,8 +120,7 @@
                                     <li><a id="btn_dashboard" class="nl disabledTab" data-toggle="tab" >Dashboard</a></li>
                                     <li><a id="btn_center" class="nl disabledTab" data-toggle="tab" >Center</a></li>
                                     <?php } ?>
-                                    <?php  if($current_page=="customer.php") { ?>
-                                         <li><a id="btn_account" class="nl disabledTab hidden" data-toggle="tab">Hidden</a></li>   
+                                    <?php  if($current_page=="customer.php") { ?>  
                                     <li><a id="btn_account" class="nl" href="#account" data-toggle="tab">Account</a></li>
                                     <li><a id="btn_provisioning" class="nl" href="#provisioning" data-toggle="tab" >Provisioning</a></li>
                                     <li><a id="btn_support" class="nl" href="#support" data-toggle="tab" >Support</a></li>
