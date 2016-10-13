f2c647ee23da7d319750d6e3844a19b8a245f06d
Jessie Enero <jessie@lumikha.co>
2016-10-13 16:49:31 +0800

    deleted some

--- liveServer.php
+++ liveServer.php
@@ -2,11 +2,9 @@
 	$live_server = false;
     if($live_server) {
         $att_path = "attachments/";
-        $tbname = 'ursa-tickets';
-        $tbid = 'ticket_id';
     } else {
         $att_path = "http://biglo.co/attachments/";
-        $tbname = 'ursa-tickets';
-        $tbid = 'ticket_id';
     }
+    $tbname = 'ursa-tickets';
+    $tbid = 'ticket_id';
 ?>
